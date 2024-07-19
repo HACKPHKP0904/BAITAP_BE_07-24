25be257e7a7493701754ed39fdb5682324fb384e
NGUYENPHIHUNG <phi160806@gmail.com>
2024-07-19 09:35:52 +0700

    Repait code

--- BAITTAPBE_01/Program.cs
+++ BAITTAPBE_01/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace BAITTAPBE_01
 {
     internal class Program
-    {  
+    {
         /*
          * Họ tên : Phi Hùng
          * Ngày viết : 19/07/2024
@@ -16,18 +16,25 @@ namespace BAITTAPBE_01
         static void Main(string[] args)
         {
             // Bài 1: Viết chương trình C# để giải bài tập tìm tổng hai số , tích 2 số , hiệu 2 số 
-            Console.OutputEncoding= Encoding.UTF8; 
-            float a, b;
+            Console.OutputEncoding = Encoding.UTF8;
+            float firstnumber, twonumber;
             Console.WriteLine("Nhập vào a :");
-            a = float.Parse(Console.ReadLine());
-            Console.WriteLine("Nhập vào b :");
-            b = float.Parse(Console.ReadLine());
-            Console.WriteLine("Tổng 2 số {0} và {1} là {2}", a, b, a + b);
-            Console.WriteLine("Hiệu 2 số {0} và {1} là {2}", a, b, a - b);
-            Console.WriteLine("Tích 2 số {0} và {1} là {2}", a, b, a * b);
-            if(b != 0)
+            while (!float.TryParse(Console.ReadLine(), out firstnumber))
             {
-                Console.WriteLine("Thương 2 số {0} và {1} là {2}", a, b, a / b);
+                Console.WriteLine("Giá trị không hợp lệ , vui lòng nhập lại a:");
+            }
+            Console.WriteLine("Nhập vào b :");
+            while (!float.TryParse(Console.ReadLine(), out twonumber))
+            {
+                Console.WriteLine("Giá trị không hợp lệ , vui lòng nhập lại b:");
+            }
+
+            Console.WriteLine("Tổng 2 số {0} và {1} là {2}", firstnumber, twonumber, firstnumber + twonumber);
+            Console.WriteLine("Hiệu 2 số {0} và {1} là {2}", firstnumber, twonumber, firstnumber - twonumber);
+            Console.WriteLine("Tích 2 số {0} và {1} là {2}", firstnumber, twonumber, firstnumber * twonumber);
+            if (twonumber != 0)
+            {
+                Console.WriteLine("Thương 2 số {0} và {1} là {2}", firstnumber, twonumber, firstnumber / twonumber);
             }
             else
             {
@@ -36,65 +43,71 @@ namespace BAITTAPBE_01
 
             //Bài 2: Viết Chương trình giải phương trình bậc 1 , bậc 2 ( 2x2 + 3x +1=0)
             Console.WriteLine("------------------------------------------------");
-            Console.WriteLine("Giải phương trình bậc nhất {0}x + {1} = 0",a,b);
-            if (a != 0)
+            Console.WriteLine("Giải phương trình bậc nhất {0}x + {1} = 0", firstnumber, twonumber);
+            if (firstnumber != 0)
             {
-                Console.WriteLine("Kết quả phương trình x = -{0} /{1}", b, a);
+                Console.WriteLine("Kết quả phương trình x = -{0} /{1}", twonumber, firstnumber);
 
             }
-            else{
+            else
+            {
                 Console.WriteLine("Không thực hiện được phép chia");
             }
             Console.WriteLine("------------------------------------------------");
             Console.WriteLine("Giải phương trình bậc 2 ax^2 + bx + c =0");
-            float c;
+            float threenumber;
             Console.WriteLine("Nhập vào số c :");
-            c = float.Parse(Console.ReadLine());
-            if (a != 0)
-            {   
-                if(b!=0)
+            while (!float.TryParse(Console.ReadLine(), out threenumber))
+            {
+                Console.WriteLine("Giá trị không hợp lệ, vui lòng nhập lại c ");
+            }
+            if (firstnumber != 0)
+            {
+                if (twonumber != 0)
                 {
-                    if(c!=0)
+                    if (threenumber != 0)
                     {
-                        double delta = (b * b - 4*a*c);
+                        double delta = (twonumber * twonumber - 4 * firstnumber * threenumber);
                         if (delta > 0)
                         {
-                            Console.WriteLine("Giá trị x1 = (-{0} + Math.Sqrt({1}))/2*{2}", b, delta, a);
-                            Console.WriteLine("Giá trị x2 = (-{0} - Math.Sqrt({1}))/2*{2}", b, delta, a);
+                            Console.WriteLine("Giá trị x1 = (-{0} + Math.Sqrt({1}))/2*{2}", twonumber, delta, firstnumber);
+                            Console.WriteLine("Giá trị x2 = (-{0} - Math.Sqrt({1}))/2*{2}", twonumber, delta, firstnumber);
 
-                        } else if (delta == 0)
+                        }
+                        else if (delta == 0)
                         {
-                            Console.WriteLine("Giá trị của x = - {0} / 2 *{1}", b, a);
+                            Console.WriteLine("Giá trị của x = - {0} / 2 *{1}", twonumber, firstnumber);
                         }
                         else
                         {
                             Console.WriteLine("Phương trình vô nghiệm");
                         }
 
-                    }else
+                    }
+                    else
                     {
                         Console.WriteLine("Phương trình ax^2 + bx =0");
                         Console.WriteLine("Nghiệm x1 = 0");
-                        Console.WriteLine("Nghiệm x2 = -{0}/{1}",b,a);
+                        Console.WriteLine("Nghiệm x2 = -{0}/{1}", twonumber, firstnumber);
                     }
                 }
                 else
                 {
                     Console.WriteLine("Phương trình bậc 2  ax^2 + c = 0");
-                    if (c != 0)
+                    if (threenumber != 0)
                     {
-                        float nghiem = c / a;
-                        if(nghiem <= 0)
+                        float nghiem = threenumber / firstnumber;
+                        if (nghiem <= 0)
                         {
-                            if(c == 0 )
+                            if (threenumber == 0)
                             {
                                 Console.WriteLine("Phương trình có 1 nghiệm thực x = 0");
                             }
-                            if(c < 0 ) 
+                            if (threenumber < 0)
                             {
                                 Console.WriteLine("Phương trình có 2 nghiệm ");
-                                Console.WriteLine("Nghiệm x1 = Math.Sqrt(-{0}/{1})",c,a);
-                                Console.WriteLine("Nghiệm x2 = -Math.Sqrt(-{0}/{1}",c,a);
+                                Console.WriteLine("Nghiệm x1 = Math.Sqrt(-{0}/{1})", threenumber, firstnumber);
+                                Console.WriteLine("Nghiệm x2 = -Math.Sqrt(-{0}/{1}", threenumber, firstnumber);
                             }
                         }
                         else
@@ -109,14 +122,15 @@ namespace BAITTAPBE_01
                 }
 
 
-            }else
+            }
+            else
             {
                 Console.WriteLine("Phương trình có dạng bx + c = 0");
-                if (b != 0)
+                if (twonumber != 0)
                 {
-                    if (c != 0)
+                    if (threenumber != 0)
                     {
-                        Console.WriteLine("Nghiệm x = -{0}/{1}", c, b);
+                        Console.WriteLine("Nghiệm x = -{0}/{1}", threenumber, twonumber);
                     }
                     else
                     {
@@ -130,23 +144,21 @@ namespace BAITTAPBE_01
             }
 
             //Bài 3: Viết chương trình C# để giải bài tập chuyển đổi độ C thành độ K và độ F trong C# 
-            float C;
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("Bài tập chuyễn đổi độ C");
+            float do_C;
             Console.WriteLine("Nhập vào độ C :");
-            C = float.Parse(Console.ReadLine());
-            if(C == 0)
+            while (!float.TryParse(Console.ReadLine(), out do_C))
             {
-                Console.WriteLine("Nhập lại độ C:");
-                C = float.Parse((string) Console.ReadLine());
+                Console.WriteLine("Giá trị không hợp lệ , vui lòng nhâp lại độ C");
             }
-            else
-            {
-                float K = 1 * C + 273;
-                Console.WriteLine("Đổi sang độ K ");
-                Console.WriteLine("Độ K = {0}",K);
-                float F = C *18 / 10 + 32;
-                Console.WriteLine("Đổi sang độ F");
-                Console.WriteLine("Độ F = {0} ",F);
-            }
+
+            float do_K = 1 * do_C + 273;
+            Console.WriteLine("Đổi sang độ K ");
+            Console.WriteLine("Độ K = {0}", do_K);
+            float do_F = do_C * 18 / 10 + 32;
+            Console.WriteLine("Đổi sang độ F");
+            Console.WriteLine("Độ F = {0} ", do_F);
             Console.ReadLine();
 
         }
